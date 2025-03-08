73415d334a86b1d76e01c467e3950b0511fb2b43
ACH-S-M <MuhamadihsanM101@gmail.com>
2025-03-08 13:49:58 +0700

    inheritacne

--- console/oop.php
+++ console/oop.php
@@ -1,30 +1,25 @@
 <?php 
 
 class hewan{
-    private $nama;
-    private $bulu;
-    private $karnivora;
-    private $mamalia;
-    private $hidup;
-    function __construct($nama,$bulu,$karnivore,$mamalia,$hidup){
+    protected $nama;
+    function __construct($nama){
         $this->nama = $nama;
-        $this->bulu = $bulu;
-        $this->karnivora = $karnivore;
-        $this->mamalia = $mamalia;
-        $this->hidup = $hidup;
-
     }
-    function namahewan(){
-     echo $this->nama."\n";
-    }
-    function buluhewan(){
-        echo $this->bulu;
+    function getnama(){
+        return $this->nama;
     }
     
 
 }
-$halo = new hewan("kucing","hitam abu abu",true,true,"di darat");
-$halo->namahewan();
-$halo->buluhewan();
+class kucing extends hewan {
+    function getnama(){
+        echo "nama kucingnya : ".parent::getnama();
+
+    }
+}
+$kucing = new kucing("belang");
+$kucing->getnama(); // inheritance 
+
+
 
 ?>
